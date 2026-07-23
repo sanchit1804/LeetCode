af668cf6a40fd70de9a3426984299c4111dc4b80
Sanchit Badyal <128458864+sanchit1804@users.noreply.github.com>
2026-07-23 15:54:41 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int far=0;
+        
         for(int i=0;i<nums.size();i++){
             if(i>far) return false;
-
+            if(i+nums[i]>=nums.size()-1) return true;
             far=max(far,i+nums[i]);
-
-            if(far>=nums.size()-1) return true;
         }
-        return false;
+        return true;
     }
 };
