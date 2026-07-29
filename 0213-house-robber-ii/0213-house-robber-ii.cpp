d015761c2e41f9740881c2217c428b8b735f8a9a
Sanchit Badyal <128458864+sanchit1804@users.noreply.github.com>
2026-07-29 17:48:59 +0530

    Time: 0 ms (100%), Space: 11 MB (19.1%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int start, int end){
-        int first=nums[start];
-        int second=max(nums[start+1],nums[start]);
+    int solve(vector<int>& nums,int left,int right){
+        if(left==right) return nums[left];
+        vector<int> dp(right-left+1,0);
+        dp[0]=nums[left];
+        dp[1]=max(nums[left+1],nums[left]);
 
-        for(int i=start+2;i<=end;i++){
-            int third=max(second,first+nums[i]);
-            first=second;
-            second=third;
+        for(int i=2;i<right-left+1;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i+left]);
         }
-        return second;
+        return dp[right-left];
     }
+
     int rob(vector<int>& nums) {
-        if (nums.size()==1) return nums[0];
-        if (nums.size()==2) return max(nums[0],nums[1]);
-        return max(solve(nums,0,nums.size()-2),solve(nums,1,nums.size()-1));
+        if(nums.size()==1) return nums[0];
+        if(nums.size()==2) return max(nums[0],nums[1]);
+        int odd=solve(nums,0,nums.size()-2);
+        int even=solve(nums,1,nums.size()-1);
+        return max(even,odd);
     }
 };
