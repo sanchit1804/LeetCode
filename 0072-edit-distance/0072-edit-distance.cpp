fafe35261d125b57521b8f90455cb0c1851146ee
Sanchit Badyal <128458864+sanchit1804@users.noreply.github.com>
2026-08-01 20:44:18 +0530

    Time: 7 ms (60.96%), Space: 13.4 MB (24.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
-        int n = word1.size();
-        int m = word2.size();
-
-
-        for(int i = 1; i <= n; i++) {
-            dp[i][0] = i;
+    int minDistance(string text1, string text2) {
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+        for(int i=1;i<=text1.size();i++){
+            dp[i][0]=i;
         }
-        for(int j = 1; j <= m; j++) {
-            dp[0][j] = j;
+        for(int j=1;j<=text2.size();j++){
+            dp[0][j]=j;
         }
-        for(int i=1;i<=word1.size();i++){
-            for(int j=1;j<=word2.size();j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=1+min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]});
+
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]}); 
             }
         }
-        return dp[word1.size()][word2.size()];
+        return dp[text1.size()][text2.size()];
     }
 };
