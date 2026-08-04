98f39899adcd2fc15291b11e810a31ecf2d4a2ff
Sanchit Badyal <128458864+sanchit1804@users.noreply.github.com>
2026-08-04 18:42:05 +0530

    Time: 0 ms (100%), Space: 74.6 MB (15.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,10 +5,8 @@ public:
         if(nums.size() == 1) return nums[0];
         int ans=nums[0];
         dp[0]=nums[0];
-        dp[1]=max(nums[1],nums[1]+nums[0]);
-        ans=max(dp[1],dp[0]);
-        for(int i=2;i<nums.size();i++){
-            dp[i]=max(nums[i],dp[i-1]+nums[i]);
+        for(int i=1;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-1],nums[i]);
             ans=max(ans,dp[i]);
         }
         return ans;
