79c8dc7b964decb0482a57a256fb176fc548ba6f
Sanchit Badyal <128458864+sanchit1804@users.noreply.github.com>
2026-08-24 23:49:12 +0530

    Time: 0 ms (100%), Space: 28.6 MB (79.59%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int DoubleDigit=0;
+        int SingleDigit=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>9) DoubleDigit+=nums[i];
+            else SingleDigit+=nums[i];
+        }
+        if(SingleDigit==DoubleDigit) return false;
+        
+        return true;
+    }
+};
